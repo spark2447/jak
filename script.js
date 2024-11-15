860af3f9a663df49bc7d29652c98055b10670074
spark2447 <callmethat1818@gmail.com>
2024-11-15 01:16:56 +0100

    w

---
+++ script.js
@@ -0,0 +1,16 @@
+const langs = ["AR" , "ENG" , "FR"];
+langs[1] = "IT";
+langs.push("ENG")
+langs.pop()
+console.log(langs);
+
+
+
+
+const redTeam = ["a" , "b" , "c"];
+const blueTeam = ["d" , "e" , "f"];
+console.log(redTeam , blueTeam);
+
+console.log(redTeam[1] , blueTeam[1]);
+
+
